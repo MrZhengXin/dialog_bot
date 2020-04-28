a6d392fe26aa0686e48c306e91bbe26fb1452f5f
MrZhengXin <34998703+MrZhengXin@users.noreply.github.com>
2020-04-28 13:18:38 +0800

    Update dialog_knowledge_explicitly_appending_heuristic_method.py

--- dialog_knowledge_explicitly_appending_heuristic_method.py
+++ dialog_knowledge_explicitly_appending_heuristic_method.py
@@ -2,42 +2,131 @@ import json
 import nltk
 import regex
 import re
+import datetime
+
+
+eq_relations = {
+    '星座': [],
+    '血型': [],
+    '属相': ['属 啥', '属 什么', ],
+    '成就': [],
+    '主演': ['谁 演 的'],
+    '类型': ['什么 歌'],
+    '评论': [],  # ['唱 的 咋样', '评价', '好听 吗'],
+    '导演': ['谁 导 的'],
+    '简介': ['介绍', '信息'],
+    '演唱': ['谁 唱 的', '主唱', '谁 的 歌'],
+    '身高': ['多高', '多 高'],
+    '体重': ['多重'],
+    '获奖': ['哪些 奖', '什么 成就'],
+    '口碑': [],
+    '生日': ['什么 时候 出生', '哪 年 出生', '哪年 出生', '哪一年 出生', '的 出生 ', '出生日期', '多会 出生'],
+    '出生地': ['哪里 的 人', '哪儿 的 人', '哪里 出生', '在 哪 出生', '哪儿 出生', '哪 的', '哪里 的 籍贯', '哪里 人', '出生 地区', '出生 在 哪里'],
+    '国家地区': ['国家 地区', '哪个 国家'],
+    '人均价格': ['人均 价格', '消费 怎么样', '人均 消费', '平均价格'],
+    '地址': ['在 哪', '具体位置'],
+    '评分': [],
+    '特色菜': [],
+    '日期': [],  # '问 日期' in goal[0]
+    '时间': [],  # '问 时间' in goal[0]
+    '新闻': ['故事'],
+    '天气': []
+}
+
+
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+        return True
+    except ValueError:
+        return False
+
+
+def check_relation(rel, qa):
+    possible_rel = (eq_relations[rel] if rel in eq_relations.keys() else []) + [rel]
+    for r in possible_rel:
+        if r in qa:
+            return True
+    return False
 
 
 def cal_score(triple, q, a):
     '''
     calculate if the triple(entity, relation, something) appears in the questions and answer
     '''
+    if len(triple[2]) == 0 or len(triple[3]) == 0:
+        return 0  # something empty like ["异灵灵异-2002", "评论", ""]
     qa = q + ' ' + a
-    score = 1 if triple[0] in qa else 0  # left entity appears
-    score += 1 if triple[1] in qa else 0  # relation appears
-    if triple[2] in qa:  # something directly appears
+    score = 1 if triple[0] in qa.replace(' ', '') else 0  # left entity appears
+    score += check_relation(triple[1], qa)  # relation appears
+    if triple[2] in a:  # something directly appears
         score += 2
     else:
-        if '评论' != triple[1]:
-            common_words = [word for word in triple[3] if (word in qa and word not in ['', ' '])]  # match by word
-        else:
-            common_words = [word for word in triple[3] if (word in a and word not in ['', ' '])]  # match by sub sentences
-        if len(common_words) / len(triple[3]) > 0.4 or (len(common_words) > 0 and triple[1] in ['评论', '出生地']):  # comments and birth places are more loose
+        common_words = [word for word in triple[3] if (word in a.split() and word not in ['', ' '])]  # match by word
+        if (triple[1] not in ['成就', '获奖'] and len(common_words) / len(triple[3]) > 0.4) or (len(common_words) > 0 and triple[1] in ['评论', '出生地']):  # comments and birth places are more loose
             score += 2
         else:
             score -= 2
 
     return score
 
+
 with open('train.json', 'r') as f:
     x = f.readlines()
 
-
-src = open('dialog_with_knowledge.src', 'w')
-tgt = open('dialog_with_knowledge.tgt', 'w')
+src = open('train_with_knowledge.src', 'w')
+tgt = open('train_with_knowledge.tgt', 'w')
 len_src = 0
 len_tgt = 0
+
+difficult_info_mask = {'身高': 'height', '体重': 'weight', '评分': 'rating', '地址': 'address',
+                       '人均价格': 'expense'}
 
 for i in x:
     i = json.loads(i)
     conversation = i['conversation']
     goal = i['goal'].split('-->')
+    kg = i['knowledge']
+
+    # entity replacement
+
+    entity_cnt = 0
+    entity_dict = dict()
+    for gi in goal:
+        gi_parts = gi.split('；')
+        for gi_part in gi_parts:
+            entity = re.findall('『[^』]*』', gi_part)
+            if len(entity) == 0:
+                continue
+            entity = entity[0][1:-1].strip()
+
+            entity_no = ''
+            if '兴趣点 推荐' in gi:
+                entity_no = 'restaurant_' + str(entity_cnt)
+                entity_cnt += 1
+            if '电影 推荐' in gi:
+                entity_no = 'movie_' + str(entity_cnt)
+                entity_cnt += 1
+            if '播放 音乐' in gi:
+                entity_no = 'song_' + str(entity_cnt)
+                entity_cnt += 1
+            if entity_no == '':
+                continue
+            entity_dict[entity] = entity_no
+
+            for j in range(len(conversation)):
+                if entity in conversation[j]:
+                    conversation[j] = conversation[j].replace(entity, entity_no)
+            for j in range(len(kg)):
+                if kg[j][0].replace(' ', '') == entity.replace(' ', ''):
+                    kg[j][0] = entity_no
+                kg[j][2] = kg[j][2].replace(entity, entity_no)
+
+
+
+
+
+
     user_first = True if 'User 主动' in goal[0] else False
     bot_hello = False
     if '寒暄' in goal[0]:
@@ -50,9 +139,17 @@ for i in x:
                 hello_info.append(i['user_profile']['年龄区间'])
         print(hello_info, file=src)
         len_src += 1
-    kg = i['knowledge']
+
+    # add reverse knowledge
     for j in range(len(kg)):
-        if kg[j][1] == '评论':
+        if kg[j][1] in ['生日', '演唱', '导演', '主演', '星座']:
+            kg.append([kg[j][2], kg[j][1], kg[j][0]])
+
+    for j in range(len(kg)):
+
+        if validate(kg[j][1]):
+            kg[j][1] = '天气'  # weather knowledge relation always in date form
+        if kg[j][1] in ['评论']:
             kg[j].append(re.split('[！？，。]', kg[j][2]))  # result example ["高中 时候 很 喜欢 他 的 歌 ", " 后面 就 只是 关注 他 的 生活 了 。"]
         else:
             if kg[j][1] == '出生地':
@@ -61,9 +158,11 @@ for i in x:
                 kg[j].append(kg[j][2].split(' '))  # match by words
 
         for k in kg[j][3]:
-            if k in ['', ' ', '，', '。', '！', '。', '~', '-', '好 的 '
-                                                            '', '啊', '哦', '']:  # unhelpful in matching
+            if k in ['', ' ', '，', '。', '！', '。', '~', '-'] or\
+            (len(kg[j][2]) > 4 and len(k) < 4 and kg[j][1] in ['评论']):  # unhelpful in matching
                 kg[j][3].remove(k)
+
+
 
     if len(goal) == 0:
         continue
@@ -71,6 +170,8 @@ for i in x:
     used_k = set()  # used string-format knowledge tuple in previous conversation
 
     for j in range(len(conversation)-1):
+        if conversation[j][0] == '[':
+            conversation[j] = conversation[j][4:]
         using_k = set()  # bot need these knowledge tuples to answer
         user_round = user_first ^ (j & 1)  # True if it were user speaking
         qa = conversation[j].strip() + ' ' + conversation[j + 1].strip()
@@ -81,17 +182,33 @@ for i in x:
             score = cal_score(k, history + ' ' + conversation[j].strip(), conversation[j+1].strip())
 
 
-            if score > max(1, cal_score(k, history, conversation[j].strip())):  # this knowledge might appear and not appear before
+            if score > max(2, cal_score(k, history, conversation[j].strip())):  # this knowledge might appear and not appear before
+                if k[1] in difficult_info_mask.keys() and k[2] in conversation[j+1]:
+                    conversation[j+1] = conversation[j+1].replace(k[2], difficult_info_mask[k[1]])
+                    # print(conversation[j+1])
+                    k[2] = difficult_info_mask[k[1]]
                 if user_round:  # we only need simulate bot
                     using_k.add(str(k[:3]))  # using sest to remove redundant tuple such as multiple celebrity birthday
-                    if score > 2:
-                        used_k.add(str(k[:3]))  # the knowledge is fully explored
-                        #  consider the celebrity birthday cases.
-                        #  First, the date today is asked and answered. However,
-                        #  the celebrity birthday tuple would also be partially matched.
-                        #  So the tuple cannot be treated as used knowledge.
-        if len(using_k) != 0 and user_round:
-            print(*using_k, end='\t', file=src)  # knowledge at the beginning of user question
+                if score > 3:
+                    used_k.add(str(k[:3]))  # the knowledge is fully explored
+                    #  consider the celebrity birthday cases.
+                    #  First, the date today is asked and answered. However,
+                    #  the celebrity birthday tuple would also be partially matched.
+                    #  So the tuple cannot be treated as used knowledge.
+
+        if j == 0 and ('问 日期' in goal[0] or '问 时间' in goal[0]):  # situation not in knowledge
+            using_k = {i['situation']}
+
+        if user_round:
+            if len(using_k) != 0:
+                print(*using_k, end='\t', file=src)  # knowledge at the beginning of user question
+            elif 0 < j < len(conversation) - 2:  # say good bye need not history information
+                add_history = ''
+                pos_h = j - 1
+                while len(add_history + conversation[pos_h]) < 128 and pos_h >= 0:
+                    add_history += conversation[pos_h].strip() + ' '
+                    pos_h -= 1
+                print(add_history, end='\t', file=src)
         print(conversation[j], file=src if user_round else tgt)
         if user_round:
             len_src += 1
